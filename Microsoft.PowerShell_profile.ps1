0833c1f213f9970cc21d67adcbb2cdfbfc63d8fc
Chris Patti <feoh@feoh.org>
2023-07-04 13:35:01 -0400

    Move openai env prompt out of main startup.
    Also add instructions to README.md

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,4 +2,3 @@ oh-my-posh init pwsh | Invoke-Expression
 # replace 'Ctrl+t' and 'Ctrl+r' with your preferred bindings:
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 # OPENAI API Key
-$Env:OPENAI_API_KEY=op item get OpenAI --fields Credential
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 This repository is a place where I can store my
 startup and other configuration files for Windows.
+
+You'll need to add $HOME\bin to your user's path.
+
+For instance I added C:\Users\feoh\bin to my user PATH
+on this Windows box.
+
+Then copy openai.ps1 to that dir.
