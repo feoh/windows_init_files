7baf8fbda80578694b34851126ed73db3e791b77
Christopher Patti <feoh@fulcrum.mit.edu>
2024-05-14 12:13:53 -0400

    Add speech synthesis to Powershell startup

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,7 @@
 oh-my-posh init pwsh | Invoke-Expression
 # replace 'Ctrl+t' and 'Ctrl+r' with your preferred bindings:
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
-# OPENAI API Key
+Set-Alias -Name 'renv' -Value 'Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1'
+# I like speech synthesis :) It's fun.
+Add-Type -AssemblyName System.speech
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
